8ef77ce24f953896958056cfe008ec4afb9e6384
x12050xx <x12050xx@Sato-MacBookAir.local>
2017-01-18 17:27:58 +0900

    トマトのヘタの色取得 カメラストリームへのトマトの対応

--- fresh_vegtables/fresh_vegtables/satou_main.cpp
+++ fresh_vegtables/fresh_vegtables/satou_main.cpp
@@ -10,7 +10,9 @@
 #include <opencv2/opencv.hpp>
 
 //画像ファイル (小さめが良い)
-#define FILE_NAME "/Users/x12050xx/Documents/git/fresh_vegtables/4305391-1-つのトマト、白いトップ、孤立した上からの眺め.jpg"
+//#define FILE_NAME "/Users/x12050xx/Documents/git/fresh_vegtables/4305391-1-つのトマト、白いトップ、孤立した上からの眺め.jpg"
+//#define FILE_NAME "/Users/x12050xx/Documents/git/fresh_vegtables/pick-best-tomatoes-15.jpg"
+#define FILE_NAME "/Users/x12050xx/Documents/git/fresh_vegtables/xxx.jpg"
 #define WINDOW_NAME_INPUT "input"
 #define WINDOW_NAME_OUTPUT "grayimage"
 #define WINDOW_NAME_BINARY "binary"
@@ -19,9 +21,9 @@
 //二値化閾値
 #define BIN_TH (100)
 //面積閾値
-#define AREA_TH (0.3)
+#define AREA_TH (0.1)
 //円形度の閾値
-#define CIRC_TH (0.8)
+#define CIRC_TH (0.50)
 
 
 //---------------------------------------------------------関数のプロトタイプ宣言
@@ -34,18 +36,55 @@ void Printf_Result_Judge_Tomato(int result);
 //メイン文
 int main(int argc, const char * argv[]) {
     
-    //画像の入力
-    //cv::Mat src_img = cv::imread(FILE_NAME, cv::IMREAD_ANYCOLOR);
-    cv::Mat src_img = cv::imread(FILE_NAME);
-    
-    //入力失敗の場合
-    if (src_img.empty()) {
-        printf("画像が読み込めませんでした\(^q^)/ \n");
-        return (-1);
+    //カメラ用変数宣言
+    cv::VideoCapture cap(0); //ビデオキャプチャ用
+    //cap.set(CV_CAP_PROP_FRAME_WIDTH, WIDTH);
+    //cap.set(CV_CAP_PROP_FRAME_HEIGHT, HEIGHT);
+    if (!cap.isOpened()) {
+        std::cerr << "Capture is not opened\n" << std::endl;
+        exit(0);
     }
+    cv::namedWindow("Capture", CV_WINDOW_AUTOSIZE | CV_WINDOW_FREERATIO);
+    cv::Mat frame;//カメラの毎フレーム格納Mat
+    int kamera_break_flag=0;
     
-    //トマトの美味しさ判別
-    Printf_Result_Judge_Tomato( Judge_Tomato(src_img) );
+    //動画像の撮影
+    while (1) {
+        cap >> frame; //キャプチャ
+        cv::imshow("Capture", frame);
+        
+        
+        int key = cv::waitKey(30);
+        
+        //キーボタンで各判定関数へ画像を入れる
+        switch (key) {
+            case 'q'://終了
+                kamera_break_flag=1;
+                break;
+                
+            case 't'://tomato の t
+                //トマトの美味しさ判別
+                Printf_Result_Judge_Tomato( Judge_Tomato(frame) );
+                break;
+                
+            case 'k'://kyuri の k
+                //きゅうりの美味しさ判別
+                //引数に画像frameを入れる関数
+                break;
+                
+            case 'n'://ninjin の t
+                //にんじんの美味しさ判別
+                //引数に画像frameを入れる関数
+                break;
+                
+            default:
+                break;
+        }
+        
+        
+        //終了
+        if(kamera_break_flag==1)break;
+    }
     
     
     return 0;
@@ -55,8 +94,8 @@ int main(int argc, const char * argv[]) {
 int Judge_Tomato(cv::Mat tomato_picture){
     
     //変数宣言
-    //1. 画像の宣言 (入力画像，グレースケール画像，二値画像，一時的な画像，出力画像,HSV用画像)
-    cv::Mat gray_img, bin_img, tmp_img, dst_img,hsv_img;
+    //1. 画像の宣言 (入力画像，グレースケール画像，二値画像，一時的な画像，出力画像,HSV用画像,ヘタの抽出マスク)
+    cv::Mat gray_img, bin_img, tmp_img, dst_img,hsv_img,heta_bin_img;
     double L, S, R;//
     //2. 輪郭の座標リストの宣言
     std::vector< std::vector< cv::Point > > contours;
@@ -64,6 +103,13 @@ int Judge_Tomato(cv::Mat tomato_picture){
     int judge=0;//1:美味しい 0:不味い
     //最大面積
     double max_s=tomato_picture.rows*tomato_picture.cols;
+    //緑色抽出
+    int green_sum=0;
+    cv::Vec3b green_hsv;
+    double result_hsv_ave[3];
+    for(int i=0;i<3;i++)result_hsv_ave[i]=0;
+    
+    int tmp_flag=0;
     
     //4. 入力画像を結果画像にコピー
     dst_img = tomato_picture.clone();
@@ -71,10 +117,13 @@ int Judge_Tomato(cv::Mat tomato_picture){
     //5. グレースケール化,HSV画像化
     cv::cvtColor(tomato_picture, gray_img, CV_BGR2GRAY);
     cv::cvtColor(tomato_picture, bin_img, CV_BGR2GRAY);
+    cv::cvtColor(tomato_picture, heta_bin_img, CV_BGR2GRAY);
     cv::cvtColor(tomato_picture, hsv_img, CV_BGR2HSV);
     
     //6. 二値化 (固定閾値で実装．閾値: 100)
     cv::Vec3b hsv;
+    
+    //赤色抽出　緑色抽出
     //画像の走査
     for (int y=0; y<tomato_picture.rows; y++) {
         for (int x=0; x<tomato_picture.cols; x++) {
@@ -87,15 +136,27 @@ int Judge_Tomato(cv::Mat tomato_picture){
             }else{
                 bin_img.at<uchar>(y, x)=0;
             }
+            
+            //緑色抽出
+            if(30<hsv[0] && hsv[0]<130 && 80<hsv[1]){
+                heta_bin_img.at<uchar>(y, x)=255;
+                green_sum++;
+                for(int i=0;i<3;i++){
+                    result_hsv_ave[i]+=(double)hsv[i];
+                }
+            }else{
+                heta_bin_img.at<uchar>(y, x)=0;
+            }
         }
     }
     
     //二値画像コピー
     tmp_img = bin_img.clone();
     
+    tmp_flag=0;
+    
     //7. 輪郭抽出
     cv::findContours(tmp_img, contours, CV_RETR_LIST, CV_CHAIN_APPROX_NONE);
-    
     //8. 輪郭の描画
     for (int i=0; i<contours.size(); i++) {
         //輪郭の長さ
@@ -104,31 +165,48 @@ int Judge_Tomato(cv::Mat tomato_picture){
         S = cv::contourArea(contours[i]);
         
         //面積が画像の1/3以上なら
-        if (max_s*AREA_TH < S) {
+        if (max_s * AREA_TH < S) {
             //円形度の計算
             R = 4.0 * M_PI * S / (L * L);
+            //printf("--%.2f--\n",R);
             
             //円形度が閾値以上なら
             if (CIRC_TH < R) {
                 //輪郭の描画
                 cv::drawContours(dst_img, contours, i, CV_RGB(0, 255, 0), 3);
                 //丸いので美味しい判定
-                judge=1;//1=美味しい
+                tmp_flag=1;//1=美味しい
             }else{
                 cv::drawContours(dst_img, contours, i, CV_RGB(0, 0, 255), 3);
-                printf("%f\n",R);
+                //printf("%f\n",R);
             }
-            
         }
-        
     }
+    if(tmp_flag==1){judge++;}
+    
     
     //9. 表示
-    cv::imshow(WINDOW_NAME_INPUT, tomato_picture);
-    cv::imshow(WINDOW_NAME_BINARY, bin_img); //二値画像は，findcontour後では使用できない
-    cv::imshow(WINDOW_NAME_OUTPUT, dst_img);
-    cv::waitKey();
+    //cv::imshow(WINDOW_NAME_INPUT, tomato_picture);
+    //cv::imshow(WINDOW_NAME_BINARY, bin_img); //二値画像は，findcontour後では使用できない
+    //cv::imshow(WINDOW_NAME_OUTPUT, heta_bin_img);//dst_img
+    //cv::waitKey();
     
+    
+    tmp_flag=0;
+    //最後にヘタの色値チェック
+    if(judge==1){
+        for(int i=0;i<3;i++){
+            result_hsv_ave[i]=(double)result_hsv_ave[i]/(double)green_sum;
+        }
+        printf("h:%.1f s:%.1f v:%.1f\n",result_hsv_ave[0],result_hsv_ave[1],result_hsv_ave[2]);
+        
+        if(55<result_hsv_ave[0] && result_hsv_ave[0]<65){
+            tmp_flag=1;
+        }else{
+            printf("色がダメ\n");
+        }
+    }
+    if(tmp_flag==1){judge++;}
     
     return judge;//1:美味しい 0:不味い
 }
@@ -138,38 +216,25 @@ void Printf_Result_Judge_Tomato(int result){
     //トマトの美味しさ判別
     if(result==1 )
     {
-        printf("おいしい\n");
+        printf("おいしいトマト\n");
+    }else if(result==2){
+        printf("すごくおいしいトマト\n");
     }
     else{
-        printf("まずい\n");
+        printf("まずいトマト\n");
     }
     
 }
 //---------------------------------------------------------
-int temp(cv::Mat tomato_picture){
+int memo(){
+    //画像の入力
+    //cv::Mat src_img = cv::imread(FILE_NAME, cv::IMREAD_ANYCOLOR);
+    cv::Mat src_img = cv::imread(FILE_NAME);
     
-    int judge;//1:美味しい 0:不味い
-    
-    //出力画像のメモリ確保
-    cv::Mat gray_img = cv::Mat(tomato_picture.size(), CV_8UC1);
-    
-    //画像の走査
-    for (int y=0; y<tomato_picture.rows; y++) {
-        for (int x=0; x<tomato_picture.cols; x++) {
-            //画素値の取得
-            cv::Vec3b s = tomato_picture.at<cv::Vec3b>(y, x);
-            uchar val = 0.11448*s[0] //B
-            + 0.58661*s[1] //G
-            + 0.29891*s[2];//R
-            gray_img.at<uchar>(y, x) = val;
-        }
+    //入力失敗の場合
+    if (src_img.empty()) {
+        printf("画像が読み込めませんでした\(^q^)/ \n");
+        return (-1);
     }
-    // OpenCV関数
-    cv::cvtColor(tomato_picture, gray_img, cv::COLOR_BGR2GRAY);
-    
-    cv::imshow(WINDOW_NAME_INPUT, tomato_picture);//画像の表示
-    cv::imshow(WINDOW_NAME_OUTPUT, gray_img);//画像の表示
-    cv::waitKey(); //キー入力待ち (止める)
-    
-    return judge;
+    return 0;
 }
